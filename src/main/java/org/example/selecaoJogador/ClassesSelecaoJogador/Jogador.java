39545817e6b877bef69e1c2517223d3d50bb00e8
João Pedro Barros Silva Azevedo <jp67.azevedo@gmail.com>
2026-04-25 08:02:29 -0300

    Pasta de Classes
    Pasta contendo as classes Selecao e Jogador

---
+++ src/main/java/org/example/selecaoJogador/ClassesSelecaoJogador/Jogador.java
@@ -0,0 +1,67 @@
+package ClassesSelecaoJogador;
+
+public class Jogador{
+    
+    private Posicao posicao;
+    private enum Posicao{
+        GOLEIRO, ZAGUEIRO, LATERAL,
+        VOLANTE, PONTA, CENTROAVANTE,
+        RESERVA, TECNICO
+    }
+    private StatusJogador status;
+    private enum StatusJogador{
+        ATIVO, LESIONADO, SUSPENSO
+    }
+    private Selecao selecao;
+    private int numero;
+    private String nome;
+    private String dataNascimento;
+
+    //Construtor
+    public Jogador(Posicao posicao) {
+        this.posicao = posicao;
+        this.status = StatusJogador.ATIVO;
+        this.selecao = null;
+        //numeracao condicional para jogador
+    }
+    
+    //Getters e Setters
+    public Posicao getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(Posicao posicao) {
+        this.posicao = posicao;
+    }
+    
+    public StatusJogador getStatus() {
+        return status;
+    }
+ 
+    public void setStatus(StatusJogador status){
+        this.status = status;
+    }
+
+    public Selecao getSelecao() {
+        return selecao;
+    }
+    
+    public void setSelecao(Selecao selecao){
+        this.selecao = selecao;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        if (numero > 0){
+            this.numero = numero;
+        }
+    }
+    
+    //Metodos Especializados
+    public int idade(){
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/selecaoJogador/ClassesSelecaoJogador/Selecao.java
@@ -0,0 +1,56 @@
+package ClassesSelecaoJogador;
+
+import java.util.LinkedList;
+
+public class Selecao {
+    private LinkedList<Jogador> time = new LinkedList<>();
+    private String nomeTecnico;
+    private int grupo;
+    private String pais;
+    public static final int MAX_MEMBROS = 26;
+    public static final int MIN_MEMBROS = 18;
+    
+
+    //Construtor
+    public Selecao(byte grupo) {
+        this.grupo = grupo;
+    }
+
+    //Getters e Setters
+    public LinkedList<Jogador> getTime() {
+        return time;
+    }
+
+    //Convocar() Talvez retirar esse setter.
+    public void setTime(LinkedList<Jogador> time) {
+        this.time = time;
+    }
+    
+    public int getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(int grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+    //Metodos especializados
+    public void removerJogador(Jogador jogador){
+    }
+    
+    public void convocarJogador(Jogador jogador){
+
+    }
+    
+    public int jogadoresAtivos(){
+        return 0;
+    }
+}
